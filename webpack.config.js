b0bd5119cd7f5783cc7b3e837ffb9ca7b9147464
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-03 23:55:05 +0300

    add HtmlWebpackPlugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -21,5 +22,10 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       }
     ],
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 };
