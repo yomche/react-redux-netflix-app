09864c8ce9edbc778f99efbdcf9203a0444334d7
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-05 13:12:42 +0300

    add prod config for webpack

---
+++ webpack.prod.config.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CompressionPlugin = require("compression-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    publicPath: '',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'prod_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true }
+          }, {
+            loader: 'postcss-loader',
+          }
+        ]
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'assets',
+        },
+      },
+      {
+        test: /\.(eot|otf|svg|ttf|woff|woff2)$/i,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "fonts",
+        }
+      }
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+    compress: false,
+    port: 3000,
+},
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new UglifyJSPlugin(),
+
+    new CompressionPlugin(),
+
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    })
+  ]
+};
