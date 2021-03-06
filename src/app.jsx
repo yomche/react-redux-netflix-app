d4916ee7d9b1074905f52f865f51fb629da59121
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-06 23:12:05 +0300

    change app.jsx path

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+import '/styles/styles.scss';
+import React from 'react';
+
+import { ArrowFuncComponent } from './components/arrowComponent.jsx';
+import { FuncComponent } from './components/funcComponent.jsx';
+import { ClassComponent } from './components/classComponent.jsx';
+import { AnotherClassComponent } from './components/anotherClassComponent.jsx';
+import { YetAnotherClassComponent } from './components/yetAnotherClassComponent.jsx';
+
+export default function App() {
+  return (
+    <div className="App">
+      <ArrowFuncComponent />
+      <FuncComponent name="React" />
+      <ClassComponent />
+      <AnotherClassComponent />
+      <YetAnotherClassComponent />
+    </div>
+  );
+}
