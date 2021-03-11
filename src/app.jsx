4d147c9c584ca6b1d2e4bf8dfb17e9a4960a458c
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-11 13:37:26 +0300

    rename components in kebab case in imports

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,11 @@
 import '../styles/styles.scss';
 import React from 'react';
 
-import ArrowFuncComponent from './components/arrowComponent';
-import FuncComponent from './components/funcComponent';
-import ClassComponent from './components/classComponent';
-import AnotherClassComponent from './components/anotherClassComponent';
-import YetAnotherClassComponent from './components/yetAnotherClassComponent';
+import ArrowFuncComponent from './components/arrow-component';
+import FuncComponent from './components/func-component';
+import ClassComponent from './components/class-component';
+import AnotherClassComponent from './components/another-class-component';
+import YetAnotherClassComponent from './components/yet-another-class-component';
 
 export default function App() {
   return (
