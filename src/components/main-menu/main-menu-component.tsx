99db469d7403f9c4937b5aa7920bb9580f0dfcb3
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-16 17:26:35 +0300

    add main menu component

---
+++ src/components/main-menu/main-menu-component.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import StyledMainMenu from './main-menu-styles';
+
+const MainMenuComponent = ():JSX.Element => <StyledMainMenu />;
+
+export default MainMenuComponent;
---
+++ src/components/main-menu/main-menu-styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledMainMenu = styled.main`
+    width: 1200px;
+    height: 1000px;
+    background-color: #d3d3d3;
+    margin: auto;
+`;
+
+export default StyledMainMenu;
