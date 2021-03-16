428c701ce1ca1c902a1fcc591d988500544a4d04
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-16 23:05:57 +0300

    update header component

--- src/components/header/header-component.tsx
+++ src/components/header/header-component.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import StyledLogo from '../logo/logo-styles';
-import StyledHeader from './header-styles';
+import {
+  StyledHeader,
+  StyledSearchButton,
+  StyledHeaderSection,
+  StyledSorter,
+} from './header-styles';
+
+import {
+  StyledSearch,
+  StyledHeading,
+  StyledSearchField,
+} from '../search/search-styles';
+
+import {
+  StyledButtonSection,
+  StyledTitleButton,
+  StyledGenreBottom,
+  StyledSubmitBottom,
+} from '../buttons/button-styles';
 
 const HeaderComponent = ():JSX.Element => (
   <StyledHeader>
-    <StyledLogo />
+    <StyledHeaderSection>
+      <StyledLogo />
+      <StyledSearchButton> SEARCH </StyledSearchButton>
+    </StyledHeaderSection>
+    <StyledSearch>
+      <StyledHeading>FIND YOUR MOVIE</StyledHeading>
+      <StyledSearchField placeholder="Quentin Tarantino" />
+    </StyledSearch>
+    <StyledButtonSection>
+      <StyledTitleButton>TITLE</StyledTitleButton>
+      <StyledGenreBottom> GENRE </StyledGenreBottom>
+      <StyledSubmitBottom> SUBMIT </StyledSubmitBottom>
+    </StyledButtonSection>
+    <StyledSorter />
   </StyledHeader>
 );
 
--- src/components/header/header-styles.js
+++ src/components/header/header-styles.js
@@ -1,13 +1,36 @@
 import styled from 'styled-components';
 import { headerBackground } from './images';
 
-const StyledHeader = styled.header`
+export const StyledHeader = styled.header`
     width: 1200px;
-    height: 250px;
+    height: 300px;
     background-image: url(${headerBackground});
-    background-color: #efca7c;
+    background-color: rgba(0, 0, 0);
     background-size: cover;
     margin: auto;
 `;
 
-export default StyledHeader;
+export const StyledHeaderSection = styled.section`
+    width: 1000px;
+    height: 55px;
+    align-items: center;
+    display: flex;
+    margin: auto;
+    justify-content:space-between;
+`;
+
+export const StyledSorter = styled.section`
+    width: 1200px;
+    height: 40px;
+    background-color: #808080;
+    margin-top: 20px;
+`;
+
+export const StyledSearchButton = styled.button`
+    width: 100px;
+    height: 30px;
+    background-color: #ffffff;
+    color: #ff0000;
+    font-weight: bold;
+    border-radius: 5px;
+`;
