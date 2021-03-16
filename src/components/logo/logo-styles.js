aa768e0f7fc2bdf48e2792e2c6ee372ebe59c81e
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-16 23:08:22 +0300

    change logo size

--- src/components/logo/logo-styles.js
+++ src/components/logo/logo-styles.js
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import { netflixLogo } from './images';
 
 const StyledLogo = styled.div`
-    width: 250px;
-    height: 70px;
+    width: 120px;
+    height: 30px;
     background-image: url(${netflixLogo});
-    background-size: cover;
+    background-size: 120px 30px;
 `;
 
 export default StyledLogo;
