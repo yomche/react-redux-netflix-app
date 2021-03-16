73a794f0fb85ee550315741dcd15c4c987b6a852
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-16 17:26:06 +0300

    add footer component

---
+++ src/components/footer/footer-component.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StyledLogo from '../logo/logo-styles';
+import StyledFooter from './footer-styles';
+
+const FooterComponent = ():JSX.Element => (
+  <StyledFooter>
+    <StyledLogo />
+  </StyledFooter>
+);
+
+export default FooterComponent;
---
+++ src/components/footer/footer-styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+    width: 1200px;
+    height: 70px;
+    background-color: #808080;
+    margin: auto;
+`;
+
+export default StyledFooter;
