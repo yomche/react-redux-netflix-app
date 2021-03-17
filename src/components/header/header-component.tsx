ddf67d8e9578e4d21ae95221102cb231f8d2c02c
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-17 17:21:38 +0300

    make restructuring of header component

--- src/components/header/header-component.tsx
+++ src/components/header/header-component.tsx
@@ -1,42 +1,14 @@
 import React from 'react';
-import StyledLogo from '../logo/logo-styles';
-import {
-  StyledHeader,
-  StyledSearchButton,
-  StyledHeaderSection,
-  StyledSorter,
-} from './header-styles';
+import { StyledHeader, StyledSorter } from './header-styles';
+import { Controls } from '../controls/controls-component';
+import { SearchInput } from '../search-input/search-input-component';
+import { NavPanel } from '../nav-panel/nav-panel-component';
 
-import {
-  StyledSearch,
-  StyledHeading,
-  StyledSearchField,
-} from '../search/search-styles';
-
-import {
-  StyledButtonSection,
-  StyledTitleButton,
-  StyledGenreBottom,
-  StyledSubmitBottom,
-} from '../buttons/button-styles';
-
-const HeaderComponent = ():JSX.Element => (
+export const HeaderComponent = ():JSX.Element => (
   <StyledHeader>
-    <StyledHeaderSection>
-      <StyledLogo />
-      <StyledSearchButton> SEARCH </StyledSearchButton>
-    </StyledHeaderSection>
-    <StyledSearch>
-      <StyledHeading>FIND YOUR MOVIE</StyledHeading>
-      <StyledSearchField placeholder="Quentin Tarantino" />
-    </StyledSearch>
-    <StyledButtonSection>
-      <StyledTitleButton>TITLE</StyledTitleButton>
-      <StyledGenreBottom> GENRE </StyledGenreBottom>
-      <StyledSubmitBottom> SUBMIT </StyledSubmitBottom>
-    </StyledButtonSection>
+    <NavPanel />
+    <SearchInput />
+    <Controls />
     <StyledSorter />
   </StyledHeader>
 );
-
-export default HeaderComponent;
