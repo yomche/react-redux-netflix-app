544cdb7fcbcb83bce9a53cdef562c7cda992a56a
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-17 17:22:57 +0300

    add curly braces

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
 import '../styles/styles.scss';
 import React from 'react';
 
-import HeaderComponent from './components/header/header-component';
-import MainMenuComponent from './components/main-menu/main-menu-component';
-import FooterComponent from './components/footer/footer-component';
+import { HeaderComponent } from './components/header/header-component';
+import { MainMenuComponent } from './components/main-menu/main-menu-component';
+import { FooterComponent } from './components/footer/footer-component';
 
 export default function App(): JSX.Element {
   return (
