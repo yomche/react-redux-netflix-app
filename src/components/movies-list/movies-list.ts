04a09e70a3deb4f552a9657c4d9d0c174331634c
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-18 22:08:31 +0300

    add movie cards mockup

---
+++ src/components/movies-list/movies-list.ts
@@ -0,0 +1,107 @@
+export const movies = [
+  {
+    "id": 337167,
+    "title": "Black Mirror",
+    "tagline": "The future is bright",
+    "vote_average": 8.8,
+    "vote_count": 1195,
+    "release_date": "2011-02-07",
+    "poster_path": "https://sun9-72.userapi.com/impf/a-V1H8zLHXqzMb3UY8yzgicFlOVZkHw4TNXeLQ/PHLkRY64ehg.jpg?size=1459x2160&quality=96&sign=1063b2565af2a89c8ae442b61427f9e7&type=album",
+    "overview": "Set in a world only minutes from our own, Black Mirror unveils how modern technologies can backfire and be used against their makers, every episode set in a slightly different reality with different characters combating different types of technologies.",
+    "budget": 55000000,
+    "revenue": 136906000,
+    "genres": [
+      "Drama",
+      "Thriller"
+    ],
+    "runtime": 106
+  },
+  {
+    "id": 181808,
+    "title": "Chilling Adventures of Sabrina",
+    "tagline": "Happy Birthday, Witch.",
+    "vote_average": 7.5,
+    "vote_count": 4732,
+    "release_date": "2018-12-13",
+    "poster_path": "https://sun9-51.userapi.com/impf/wTwDr3Eh4cZlJPKHiHp90nPQlTlkGfPsAfqFaQ/7VcL1Zco3C4.jpg?size=1458x2160&quality=96&sign=4b7d9e8ab01d00a262d358f8a2e82534&type=album",
+    "overview": "Reimagines the origin and adventures of Sabrina: the Teenage Witch as a dark coming-of-age story that traffics in horror, the occult and, of course, witchcraft. Tonally in the vein of Rosemary's Baby and The Exorcist, this adaptation finds Sabrina Spellman wrestling to reconcile her dual nature - half-witch, half-mortal - while standing against the evil forces that threaten her, her family and the daylight world humans inhabit.",
+    "budget": 200000000,
+    "revenue": 1325937250,
+    "genres": [
+      "Fantasy",
+      "Drama"
+    ],
+    "runtime": 152
+  },
+  {
+    "id": 284054,
+    "title": "The Witcher",
+    "tagline": "I forge my own path. Bleed from my own mistakes. Destiny plays no part.",
+    "vote_average": 8.2,
+    "vote_count": 3788,
+    "release_date": "2019-02-13",
+    "poster_path": "https://sun9-42.userapi.com/impf/P71f5UItZ2ekG0PzCp16UgWLms_GxmWBzDjREQ/nEkurXb2xWE.jpg?size=1458x2160&quality=96&sign=484ff49b495837b36cf31564231e99ba&type=album",
+    "overview": "The Witcher is an American fantasy drama web television series created by Lauren Schmidt Hissrich for Netflix. It is based on the book series of the same name by Polish writer Andrzej Sapkowski. The Witcher follows the story of Geralt of Rivia, a solitary monster hunter, who struggles to find his place in a world where people often prove more wicked than monsters and beasts. But when destiny hurtles him toward a powerful sorceress, and a young princess with a special gift, the three must learn to navigate independently the increasingly volatile Continent.",
+    "budget": 200000000,
+    "revenue": 1245257672,
+    "genres": [
+      "Adventure",
+      "Fantasy"
+    ],
+    "runtime": 134
+  },
+  {
+    "id": 354912,
+    "title": "Lucifer",
+    "tagline": "It's Good to be Bad.",
+    "vote_average": 8.1,
+    "vote_count": 3619,
+    "release_date": "2016-10-27",
+    "poster_path": "https://sun9-23.userapi.com/impf/dwlJCLctnP1oWjfQgwd5DD8tbrLrVs-ItwFC7Q/0ZTahWgJM28.jpg?size=1458x2160&quality=96&sign=8a0970c4a845476449a08fd41c757113&type=album",
+    "overview": "Lucifer Morningstar, bored from his sulking life in hell, comes to live in Los Angeles. While there, he helps humanity with its miseries through his experience and telepathic abilities to bring people's deepest desires and thoughts out of them. While meeting with a Detective in his nightclub (Lux), a shootout involving him and the Detective leads him to become an LAPD consultant who tries to punish people for their crimes through law and justice.Lucifer Morningstar, bored from his sulking life in hell, comes to live in Los Angeles. While there, he helps humanity with its miseries through his experience and telepathic abilities to bring people's deepest desires and thoughts out of them. While meeting with a Detective in his nightclub (Lux), a shootout involving him and the Detective leads him to become an LAPD consultant who tries to punish people for their crimes through law and justice.",
+    "budget": 175000000,
+    "revenue": 700920729,
+    "genres": [
+      "Crime",
+      "Drama",
+      "Fantasy"
+    ],
+    "runtime": 105
+  },
+  {
+    "id": 333339,
+    "title": "13 Reasons Why",
+    "tagline": "If you're listening, you're too late.",
+    "vote_average": 7.6,
+    "vote_count": 617,
+    "release_date": "2017-03-28",
+    "poster_path": "https://sun9-70.userapi.com/impf/04EYks1a0yqG63A83UUMxJm-KABLL2tt-6zYig/oHjo_3Nij-k.jpg?size=1458x2160&quality=96&sign=0504715357119f57537181a50e4e3aca&type=album",
+    "overview": "Thirteen Reasons Why, based on the best-selling books by Jay Asher, follows teenager Clay Jensen (Dylan Minnette) as he returns home from school to find a mysterious box with his name on it lying on his porch. Inside he discovers a group of cassette tapes recorded by Hannah Baker (Katherine Langford) -his classmate and crush-who tragically committed suicide two weeks earlier. On tape, Hannah unfolds an emotional audio diary, detailing the thirteen reasons why she decided to end her life. Through Hannah and Clay's dual narratives, Thirteen Reasons Why weaves an intricate and heartrending story of confusion and desperation that will deeply affect viewers.",
+    "budget": 175000000,
+    "revenue": 0,
+    "genres": [
+      "Drama",
+      "Mystery",
+      "Thriller"
+    ],
+    "runtime": 140
+  },
+  {
+    "id": 338970,
+    "title": "You",
+    "tagline": "Meet your match",
+    "vote_average": 7.7,
+    "vote_count": 817,
+    "release_date": "2018-03-08",
+    "poster_path": "https://sun9-64.userapi.com/impf/-Mlg00N-VYWlmJBv5SMrP07uwXGlYNuYZqallQ/wY6S5EYbF5Y.jpg?size=1080x1350&quality=96&sign=cd2f4baa9ce295ec7aa5ef55a9b6232a&type=album",
+    "overview": "Based on Caroline Kepnes' best-selling novel of the same name, YOU is a 21st century love story that asks, What would you do for love? When a brilliant bookstore manager crosses paths with an aspiring writer, his answer becomes clear: anything. Using the internet and social media as his tools to gather the most intimate of details and get close to her, a charming and awkward crush quickly becomes obsession as he quietly and strategically removes every obstacle - and person - in his way.",
+    "budget": 94000000,
+    "revenue": 126025000,
+    "genres": [
+      "Crime",
+      "Drama",
+      "Romance"
+    ],
+    "runtime": 118
+  }
+];
