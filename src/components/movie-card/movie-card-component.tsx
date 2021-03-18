09daa9a2765479b8176fff735e89dee12ee6ada9
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-18 22:07:26 +0300

    add movie card component

---
+++ src/components/movie-card/movie-card-component.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { StyledMovieCard, StyledMovieCardTitle, MovieCardPoster, StyledMovieCardGenre, StyledMovieCardDate, StyledMovieCardInfo } from './movie-card-styles';
+
+export const MovieCard: FC <{ poster:string, title:string, genre:string, date:string }>= ({ poster, title, genre, date }) => {
+  return (
+    <StyledMovieCard>
+      <MovieCardPoster src = { poster } />
+      <StyledMovieCardTitle> { title } </StyledMovieCardTitle>
+      <StyledMovieCardInfo>
+        <StyledMovieCardGenre> { genre } </StyledMovieCardGenre>
+        <StyledMovieCardDate> { date } </StyledMovieCardDate>
+      </StyledMovieCardInfo>
+    </StyledMovieCard>
+  )
+};
---
+++ src/components/movie-card/movie-card-styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+const FONT_COLOR = '#000000';
+const SECONDARY_FONT_COLOR = '#808080';
+
+export const StyledMovieCard = styled.section`
+    width: 250px;
+    height: 400px;
+`;
+
+export const MovieCardPoster = styled.img`
+    width: 250px;
+    height: 350px;
+`;
+
+export const StyledMovieCardTitle = styled.h3`
+    color: ${FONT_COLOR};
+    font-weight: bold;
+    font-size: 16px;
+    text-align: left;
+    text-transform: uppercase;
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+`;
+
+export const StyledMovieCardGenre = styled.p`
+    color: ${FONT_COLOR};
+    font-size: 13px;
+    margin-top: -15px;
+    text-align: left;
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+`;
+
+export const StyledMovieCardDate = styled.p`
+    color: ${SECONDARY_FONT_COLOR};
+    width: 100px;
+    font-size: 13px;
+    margin-top: -15px;
+    text-align: center;
+    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+    border: 2px solid ${SECONDARY_FONT_COLOR};
+`;
+
+export const StyledMovieCardInfo = styled.div `
+    width: 250px;
+    height: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
