2c2ae05f5e7ac35d19aeeb85359724e2d2032054
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-19 01:25:42 +0300

    update movie list component

--- src/components/movies-list/movies-list-component.tsx
+++ src/components/movies-list/movies-list-component.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from 'react';
 import { StyledMoviesList } from './movies-list-styles';
 import { MovieCard } from '../movie-card/movie-card-component';
-import { movies } from './movies-list';
+import { movies } from '../../movies-list';
 
 export const MoviesList: FC = () => (
   <StyledMoviesList>
     {movies.map((movie) => (
-      <MovieCard 
-        poster ={ movie.poster_path }
-        title = { movie.title } 
-        genre = { movie.genres[0] }
-        date = { movie.release_date }
-        key = { movie.id } 
+      <MovieCard
+        poster={movie.poster_path}
+        title={movie.title}
+        genre={movie.genres[0]}
+        date={movie.release_date}
+        key={movie.id}
       />
-      ))}
+    ))}
   </StyledMoviesList>
 );
