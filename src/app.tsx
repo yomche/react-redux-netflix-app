46eacdb10019b0efd8489e3aa2e42a7a6e776acf
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-19 01:26:21 +0300

    update app

--- src/app.tsx
+++ src/app.tsx
@@ -4,13 +4,16 @@ import React, { FC } from 'react';
 import { Header } from './components/header/header-component';
 import { MoviesList } from './components/movies-list/movies-list-component';
 import { Footer } from './components/footer/footer-component';
+import { MoviesSorter } from './components/movies-sorter/movies-sorter-component';
 
-export const App: FC = () => {
-  return (
-    <>
-      <Header />
-      <MoviesList />
-      <Footer />
-    </>
-  );
-};
+import { MovieFullInfo } from './components/movie-full-info/movie-full-info-component';
+
+export const App: FC = () => (
+  <>
+    {/* <MovieFullInfo /> */}
+    <Header />
+    <MoviesSorter />
+    <MoviesList />
+    <Footer />
+  </>
+);
