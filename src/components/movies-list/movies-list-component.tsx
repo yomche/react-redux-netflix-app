bec99826086b051a760d41868c72aa4902cf3920
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-18 22:09:09 +0300

    update movies list component

--- src/components/movies-list/movies-list-component.tsx
+++ src/components/movies-list/movies-list-component.tsx
@@ -1,14 +1,18 @@
 import React, { FC } from 'react';
-import { StyledMoviesList, MoviesListElement } from './movies-list-styles';
+import { StyledMoviesList } from './movies-list-styles';
+import { MovieCard } from '../movie-card/movie-card-component';
+import { movies } from './movies-list';
 
 export const MoviesList: FC = () => (
   <StyledMoviesList>
-    <MoviesListElement />
-    <MoviesListElement />
-    <MoviesListElement />
-    <MoviesListElement />
-    <MoviesListElement />
-    <MoviesListElement />
-    <MoviesListElement />
+    {movies.map((movie) => (
+      <MovieCard 
+        poster ={ movie.poster_path }
+        title = { movie.title } 
+        genre = { movie.genres[0] }
+        date = { movie.release_date }
+        key = { movie.id } 
+      />
+      ))}
   </StyledMoviesList>
 );
--- src/components/movies-list/movies-list-styles.ts
+++ src/components/movies-list/movies-list-styles.ts
@@ -1,24 +1,13 @@
 import styled from 'styled-components';
 
-const BACKGROUND_COLOR = '#e6e6e6';
-const SECONDARY_BACKGROUND_COLOR = '#b1b3b1';
-const BORDER_COLOR = '#000000';
-
 export const StyledMoviesList = styled.main`
-    width: 1200px;
+    width: 1000px;
     margin: auto;
     padding-top: 30px;
-    padding-bottom: 30px;
-    background-color: ${BACKGROUND_COLOR};
+    padding-bottom: 50px;
     display: flex;
     justify-content: space-around;
     flex-wrap: wrap;
     row-gap: 60px;
-`;
-
-export const MoviesListElement = styled.section`
-    width: 300px;
-    height: 400px;
-    background-color: ${SECONDARY_BACKGROUND_COLOR};
-    border: 2px solid ${BORDER_COLOR};
+    column-gap: 100px;
 `;
