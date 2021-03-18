9576a4f077aed44dc69b74408b8df21c0804b4df
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-19 01:25:06 +0300

    update movie card component

--- src/components/movie-card/movie-card-component.tsx
+++ src/components/movie-card/movie-card-component.tsx
@@ -1,15 +1,29 @@
 import React, { FC } from 'react';
-import { StyledMovieCard, StyledMovieCardTitle, MovieCardPoster, StyledMovieCardGenre, StyledMovieCardDate, StyledMovieCardInfo } from './movie-card-styles';
+import {
+  StyledMovieCard, StyledMovieCardTitle, MovieCardPoster, StyledMovieCardGenre, StyledMovieCardDate, StyledMovieCardInfo,
+} from './movie-card-styles';
 
-export const MovieCard: FC <{ poster:string, title:string, genre:string, date:string }>= ({ poster, title, genre, date }) => {
-  return (
-    <StyledMovieCard>
-      <MovieCardPoster src = { poster } />
-      <StyledMovieCardTitle> { title } </StyledMovieCardTitle>
-      <StyledMovieCardInfo>
-        <StyledMovieCardGenre> { genre } </StyledMovieCardGenre>
-        <StyledMovieCardDate> { date } </StyledMovieCardDate>
-      </StyledMovieCardInfo>
-    </StyledMovieCard>
-  )
-};
+export const MovieCard: FC <{ poster:string, title:string, genre:string, date:string }> = ({
+  poster, title, genre, date,
+}) => (
+  <StyledMovieCard>
+    <MovieCardPoster src={poster} />
+    <StyledMovieCardTitle>
+      {' '}
+      { title }
+      {' '}
+    </StyledMovieCardTitle>
+    <StyledMovieCardInfo>
+      <StyledMovieCardGenre>
+        {' '}
+        { genre }
+        {' '}
+      </StyledMovieCardGenre>
+      <StyledMovieCardDate>
+        {' '}
+        { date }
+        {' '}
+      </StyledMovieCardDate>
+    </StyledMovieCardInfo>
+  </StyledMovieCard>
+);
