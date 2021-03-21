f15fe48099c35d4ac777eae9666ba0c9927224c7
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-22 01:16:27 +0300

    rename styled components

--- src/components/search-input/search-input.styles.ts
+++ src/components/search-input/search-input.styles.ts
@@ -4,23 +4,23 @@ const BACKGROUND_COLOR = '#000000';
 const FONT_COLOR = '#ffffff';
 const BORDER_COLOR = '#ff0000';
 
-export const StyledSearchSection = styled.form`
-    width: 1000px;
-    margin: auto;
+export const StyledSearchForm = styled.form`
+  width: 1000px;
+  margin: auto;
 `;
 
 export const StyledHeading = styled.h1`
-    color: ${FONT_COLOR};
-    font-weight: bold;
-    font-size: 35px;
-    font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+  color: ${FONT_COLOR};
+  font-weight: bold;
+  font-size: 35px;
+  font-family: 'Lucida Sans Unicode', 'Lucida Grande', sans-serif;
 `;
 
-export const StyledSearchField = styled.input`
-    width: 1000px;
-    height: 30px;
-    font-size: 20px;
-    color: ${FONT_COLOR};
-    background-color: ${BACKGROUND_COLOR};
-    border-bottom: 2px solid ${BORDER_COLOR};
+export const StyledSearchInput = styled.input`
+  width: 1000px;
+  height: 30px;
+  font-size: 20px;
+  color: ${FONT_COLOR};
+  background-color: ${BACKGROUND_COLOR};
+  border-bottom: 2px solid ${BORDER_COLOR};
 `;
