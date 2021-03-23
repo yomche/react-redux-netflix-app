98e9f15d487e8092e61596b792629397db6c67ee
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-23 17:35:02 +0300

    add storybook addon-knobs

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -4,6 +4,9 @@ import { Story } from '@storybook/react';
 
 import { Header } from './header.component';
 
+import { withKnobs, optionsKnob as options } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+
 export default {
   title: 'Components/Header',
   component: Header,
@@ -11,5 +14,24 @@ export default {
 
 const Template: Story<ComponentProps<typeof Header>> = (args) => <Header {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {};
+export const MovieList = Template.bind({});
+MovieList.args = {
+  viewType: 'movieList',
+};
+
+export const MovieFullInfo = Template.bind({});
+MovieFullInfo.args = {
+  viewType: 'movieFullInfo',
+};
+
+storiesOf('Header with Knobs', module)
+  .addDecorator(withKnobs)
+  .add('different view types', () => {
+    const viewType = {
+      movieList: 'movieList',
+      MovieFullInfo: 'movieFullInfo',
+    };
+    const viewTypeSelect = options('viewType', viewType, 'movieFullInfo', { display: 'select' });
+
+    return <Header viewType={viewTypeSelect} />;
+  });
