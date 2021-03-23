9ccaedccc1bfa295a039568b9cea3c8b9de7b4c8
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-03-23 20:56:03 +0300

    add styles

--- styles/global-styles.ts
+++ styles/global-styles.ts
@@ -3,5 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Lucida Sans Unicode', 'Lucida Grande', sans-serif;
+    margin: 0;
+    padding: 0;
   }
 `;
