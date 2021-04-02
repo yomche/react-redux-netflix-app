28d5df8448498a25e79dd186497f25e990472ae0
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-02 17:18:07 +0300

    add actions

---
+++ src/actions/index.ts
@@ -0,0 +1,19 @@
+export const setViewType = {
+  type: 'SET_VIEW_TYPE',
+  payload: '',
+};
+
+export const setCurrentMovie = {
+  type: 'SET_CURRENT_MOVIE',
+  payload: '',
+};
+
+export const setMovieFullInfoType = {
+  type: 'SET_MOVIE_FULL_INFO_TYPE',
+  payload: '',
+};
+
+export const setMovieListType = {
+  type: 'SET_MOVIE_LIST_TYPE',
+  payload: '',
+};
