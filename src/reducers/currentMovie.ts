fea2a82d3d0b958628b23c67f5320149aeb9424a
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-06 14:05:25 +0300

    add currentMovie reducer

---
+++ src/reducers/currentMovie.ts
@@ -0,0 +1,15 @@
+import { currentMovie } from '../actions/action-types';
+
+export const initialState = {
+  currentMovie: {},
+};
+
+export function currentMovieReducer(
+  state = initialState,
+  action: { type: string; payload: string }
+): Record<string, unknown> {
+  if (action.type === currentMovie) {
+    return { ...state, currentMovie: action.payload };
+  }
+  return state;
+}
