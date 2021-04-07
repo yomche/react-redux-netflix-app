2c9462195664a19153e74a15bad32d37bb315cb1
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-07 14:50:40 +0300

    add fetch action types

--- src/actions/action-types.ts
+++ src/actions/action-types.ts
@@ -1,2 +1,7 @@
 export const viewType = 'VIEW_TYPE';
+
 export const currentMovie = 'CURRENT_MOVIE';
+
+export const fetchRequest = 'FETCH_MOVIE_REQUEST';
+export const fetchSuccess = 'FETCH_MOVIES_SUCCESS';
+export const fetchFail = 'FETCH_MOVIES_FAIL';
