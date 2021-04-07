a847389e571026eea7ac1440cf78f64472694ebe
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-07 14:55:37 +0300

    update app

--- src/containers/app.tsx
+++ src/containers/app.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback, useEffect } from 'react';
 import { useDispatch, connect } from 'react-redux';
 
 import { typeOfView } from '../constants/app.constants';
@@ -8,7 +8,7 @@ import { MoviesList } from '../components/movies-list/movies-list.component';
 import { Footer } from '../components/footer/footer.component';
 import { MoviesSorter } from '../components/movies-sorter/movies-sorter.component';
 import { ErrorBoundaryMoviesList } from './error-boundary';
-import { setViewType, setCurrentMovie } from '../actions';
+import { setViewType, setCurrentMovie, fetchMovies } from '../actions';
 
 type AppProps = {
   viewType: string;
@@ -32,6 +32,10 @@ const AppComponent: FC<AppProps> = ({ viewType, currentMovie }) => {
   );
   const setMovieListType = useCallback(() => dispatch(setViewType(typeOfView.movieList)), []);
 
+  const moviesData = useEffect(() => {
+    dispatch(fetchMovies());
+  }, []);
+
   return (
     <>
       <GlobalStyle />
@@ -42,16 +46,21 @@ const AppComponent: FC<AppProps> = ({ viewType, currentMovie }) => {
       />
       <MoviesSorter />
       <ErrorBoundaryMoviesList>
-        <MoviesList onSetMovieFullInfoType={setMovieFullInfoType} />
+        <MoviesList onSetMovieFullInfoType={setMovieFullInfoType} moviesData={moviesData} />
       </ErrorBoundaryMoviesList>
       <Footer />
     </>
   );
 };
 
-const mapStateToProps = (state: { viewType: string; currentMovie: Record<string, unknown> }) => ({
-  viewType: state.viewType,
-  currentMovie: state.currentMovie,
+const mapStateToProps = (state: {
+  viewType: string;
+  currentMovie: Record<string, unknown>;
+  viewTypeData: Record<string, unknown>;
+  currentMovieData: Record<string, unknown>;
+}) => ({
+  viewType: state.viewTypeData.viewType,
+  currentMovie: state.currentMovieData.currentMovie,
 });
 
 export const App = connect(mapStateToProps)(AppComponent);
