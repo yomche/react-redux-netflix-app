a02ce0b75463ec9cc30cfe4585ce1e19f4c008a4
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-07 14:55:15 +0300

    add action creator fetch movies

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,4 @@
-import { viewType, currentMovie } from './action-types';
+import { viewType, currentMovie, fetchRequest, fetchSuccess, fetchFail } from './action-types';
 
 export function setViewType(payload: string): Record<string, unknown> {
   return { type: viewType, payload };
@@ -7,3 +7,18 @@ export function setViewType(payload: string): Record<string, unknown> {
 export function setCurrentMovie(payload: Record<string, unknown>): Record<string, unknown> {
   return { type: currentMovie, payload };
 }
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const fetchMovies = () => (dispatch: any) => {
+  dispatch({ type: fetchRequest });
+
+  const moviesUrl = 'http://react-cdp-api.herokuapp.com/movies';
+
+  fetch(moviesUrl)
+    .then((response) => response.json())
+    .then((moviesData) => {
+      console.log(moviesData);
+      return dispatch({ type: fetchSuccess, payload: moviesData });
+    })
+    .catch((error) => dispatch({ type: fetchFail, payload: error }));
+};
