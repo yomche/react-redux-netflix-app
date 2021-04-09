d214619208f6671a4ef07fe780c9ce1090e09b73
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-10 00:24:17 +0300

    remove space

--- src/actions/action-types.ts
+++ src/actions/action-types.ts
@@ -1,6 +1,3 @@
 export const viewType = 'VIEW_TYPE';
-
 export const currentMovie = 'CURRENT_MOVIE';
-
 export const fetchSuccess = 'FETCH_MOVIES_SUCCESS';
-export const fetchFail = 'FETCH_MOVIES_FAIL';
