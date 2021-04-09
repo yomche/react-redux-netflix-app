8e8dd09b1a0a4c866f8fcd20c4a77ab41b96bff3
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-09 18:43:33 +0300

    add dataType

--- src/reducers/movies-data.ts
+++ src/reducers/movies-data.ts
@@ -1,19 +1,17 @@
-import { fetchSuccess, fetchFail } from '../actions/action-types';
+import { fetchSuccess } from '../actions/action-types';
+import { dataType } from '../constants/app.constants';
 
-const initialState = {
-  posterPath: '',
-  title: '',
-  releaseDate: '',
-  overview: '',
-  id: 0,
+const initialState: dataType = {
+  data: [{ poster_path: '', title: '', release_date: '', genres: '', overview: '', id: 0 }],
 };
 
-export const moviesData = (state = initialState, action: { type: string; payload: any }) => {
+export const moviesData = (
+  state = initialState,
+  action: { type: string; payload: dataType }
+): dataType => {
   switch (action.type) {
     case fetchSuccess:
       return action.payload;
-    case fetchFail:
-      return { ...state, error: action.payload };
     default:
       return state;
   }
