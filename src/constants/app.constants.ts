889adffc39b22bff93fdc8681bcf7433c2e06cf4
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-09 18:39:44 +0300

    add types

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -1,4 +1,26 @@
+// eslint-disable-next-line no-shadow
 export enum typeOfView {
   movieList = 'movieList',
   movieFullInfo = 'movieFullInfo',
 }
+
+export type dataType = {
+  data: [moviesDataType];
+};
+
+export type moviesDataType = {
+  poster_path: string;
+  title: string;
+  release_date: string;
+  genres: string;
+  overview: string;
+  id: number;
+};
+
+export type currentMovieType = {
+  posterPath: string;
+  title: string;
+  releaseDate: string;
+  overview: string;
+  id: number;
+};
