d8a2d788d0bc687e58b0e76d3fd16eb0d85a5aa7
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-09 10:49:31 +0300

    fix moviesData reducer

--- src/reducers/movies-data.ts
+++ src/reducers/movies-data.ts
@@ -8,11 +8,10 @@ const initialState = {
   id: 0,
 };
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const moviesData = (state = initialState, action: { type: string; payload: unknown }) => {
+export const moviesData = (state = initialState, action: { type: string; payload: any }) => {
   switch (action.type) {
     case fetchSuccess:
-      return { ...state };
+      return action.payload;
     case fetchFail:
       return { ...state, error: action.payload };
     default:
