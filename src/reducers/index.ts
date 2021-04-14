b0924d7f51f56cbe11d5b7209c16fd621216ed54
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-14 10:46:09 +0300

    add search Input reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { moviesData } from './movies-data';
 import { viewTypeData } from './view-type';
 import { currentMovieData } from './current-movie';
+import { searchInputValueData } from './search-input';
 
 export const rootReducer = combineReducers({
   moviesData,
   viewTypeData,
   currentMovieData,
+  searchInputValueData,
 });
