dadb7e6dbc8178b84e47e88b6290a9fb42691592
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-14 14:24:26 +0300

    add final form

--- src/components/search-input/search-input.component.tsx
+++ src/components/search-input/search-input.component.tsx
@@ -1,26 +1,41 @@
 import React, { FC } from 'react';
-import { StyledSearchForm, StyledHeading, StyledSearchInput } from './search-input.styles';
+import { Form } from 'react-final-form';
+import {
+  StyledSearchForm,
+  StyledHeading,
+  StyledSearchInput,
+  StyledControlsSection,
+  StyledTitleButton,
+  StyledGenreButton,
+  StyledSubmitButton,
+} from './search-input.styles';
 
 type SearchInputProps = {
   inputValue: string;
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const SearchInput: FC<SearchInputProps> = ({ handleChange, inputValue }) => {
-  const handleSubmit = (event: { preventDefault: () => void }) => {
-    // eslint-disable-next-line no-console
-    console.log(`Введенный фильм: ${inputValue}`);
-    event.preventDefault();
-  };
-  return (
-    <StyledSearchForm onSubmit={handleSubmit}>
-      <StyledHeading>FIND YOUR MOVIE</StyledHeading>
-      <StyledSearchInput
-        type="text"
-        value={inputValue}
-        placeholder="Quentin Tarantino"
-        onChange={handleChange}
-      />
-    </StyledSearchForm>
-  );
-};
+export const SearchInput: FC<SearchInputProps> = ({ handleChange, inputValue }) => (
+  <Form
+    onSubmit={() => {
+      // eslint-disable-next-line no-console
+      console.log(`Введенный фильм: ${inputValue}`);
+    }}
+    render={({ handleSubmit }) => (
+      <StyledSearchForm onSubmit={handleSubmit}>
+        <StyledHeading>FIND YOUR MOVIE</StyledHeading>
+        <StyledSearchInput
+          type="text"
+          value={inputValue}
+          placeholder="Quentin Tarantino"
+          onChange={handleChange}
+        />
+        <StyledControlsSection>
+          <StyledTitleButton> TITLE </StyledTitleButton>
+          <StyledGenreButton> GENRE </StyledGenreButton>
+          <StyledSubmitButton> SUBMIT </StyledSubmitButton>
+        </StyledControlsSection>
+      </StyledSearchForm>
+    )}
+  />
+);
