ddfeecc66931f32652fd4c3e81b40d0ef85393ec
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-14 14:25:18 +0300

    fix search input styles

--- src/components/search-input/search-input.styles.ts
+++ src/components/search-input/search-input.styles.ts
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 const BACKGROUND_COLOR = '#000000';
 const FONT_COLOR = '#ffffff';
 const BORDER_COLOR = '#ff0000';
+const BUTTON_BACKGROUND_COLOR = '#ff0000';
+const BUTTON_COLOR = '#a9a9a9';
 
 export const StyledSearchForm = styled.form`
   width: 1000px;
@@ -22,4 +24,55 @@ export const StyledSearchInput = styled.input`
   color: ${FONT_COLOR};
   background-color: ${BACKGROUND_COLOR};
   border-bottom: 2px solid ${BORDER_COLOR};
+`;
+
+export const StyledControlsSection = styled.section`
+  width: 1000px;
+  height: 70px;
+  margin: auto;
+  display: flex;
+  align-items: center;
+`;
+
+export const StyledTitleButton = styled.button`
+  width: 100px;
+  height: 30px;
+  color: ${FONT_COLOR};
+  background-color: ${BUTTON_BACKGROUND_COLOR};
+  margin-right: 15px;
+  border-radius: 5px;
+
+  :hover {
+    cursor: pointer;
+    background-color: ${BUTTON_COLOR};
+  }
+`;
+
+export const StyledGenreButton = styled.button`
+  width: 100px;
+  height: 30px;
+  color: ${FONT_COLOR};
+  background-color: ${BUTTON_BACKGROUND_COLOR};
+  border-radius: 5px;
+  margin-right: 650px;
+
+  :hover {
+    cursor: pointer;
+    background-color: ${BUTTON_COLOR};
+  }
+`;
+
+export const StyledSubmitButton = styled.button`
+  width: 160px;
+  height: 35px;
+  font-size: 17px;
+  color: ${FONT_COLOR};
+  background-color: ${BUTTON_BACKGROUND_COLOR};
+  margin-top: 20px;
+  border-radius: 5px;
+
+  :hover {
+    cursor: pointer;
+    background-color: ${BUTTON_COLOR};
+  }
 `;
