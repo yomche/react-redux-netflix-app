b7adfa12aa6823644f943079a1b06ff07c15ea73
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-14 10:59:34 +0300

    add getting input data from a form

--- src/components/search-input/search-input.component.tsx
+++ src/components/search-input/search-input.component.tsx
@@ -1,9 +1,26 @@
 import React, { FC } from 'react';
 import { StyledSearchForm, StyledHeading, StyledSearchInput } from './search-input.styles';
 
-export const SearchInput: FC = () => (
-  <StyledSearchForm>
-    <StyledHeading>FIND YOUR MOVIE</StyledHeading>
-    <StyledSearchInput type="text" placeholder="Quentin Tarantino" />
-  </StyledSearchForm>
-);
+type SearchInputProps = {
+  inputValue: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const SearchInput: FC<SearchInputProps> = ({ handleChange, inputValue }) => {
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    // eslint-disable-next-line no-console
+    console.log(`Введенный фильм: ${inputValue}`);
+    event.preventDefault();
+  };
+  return (
+    <StyledSearchForm onSubmit={handleSubmit}>
+      <StyledHeading>FIND YOUR MOVIE</StyledHeading>
+      <StyledSearchInput
+        type="text"
+        value={inputValue}
+        placeholder="Quentin Tarantino"
+        onChange={handleChange}
+      />
+    </StyledSearchForm>
+  );
+};
