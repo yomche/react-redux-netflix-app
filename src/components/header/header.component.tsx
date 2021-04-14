07d8a5c4ee8a45f06b3d00f4db8aae1ef3c35a80
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-14 10:57:04 +0300

    update component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -4,20 +4,29 @@ import { Controls } from '../controls/controls.component';
 import { SearchInput } from '../search-input/search-input.component';
 import { NavPanel } from '../nav-panel/nav-panel.component';
 import { MovieFullInfo } from '../movie-full-info/movie-full-info.component';
-import { typeOfView, currentMovieType } from '../../constants/app.constants';
+import { TypeOfView } from '../../constants/app.constants';
+import { CurrentMovieType } from '../../types';
 
 type HeaderProps = {
   viewType: string;
   onSetMovieListType: () => void;
-  currentMovie: currentMovieType;
+  currentMovie: CurrentMovieType;
+  searchInputValue: string;
+  onSetSearchInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const Header: FC<HeaderProps> = ({ viewType, currentMovie, onSetMovieListType }) => (
+export const Header: FC<HeaderProps> = ({
+  viewType,
+  currentMovie,
+  onSetMovieListType,
+  onSetSearchInputChange,
+  searchInputValue,
+}) => (
   <StyledHeader>
     <NavPanel onClickToChange={onSetMovieListType} />
-    {viewType === typeOfView.movieList ? (
+    {viewType === TypeOfView.movieList ? (
       <>
-        <SearchInput />
+        <SearchInput handleChange={onSetSearchInputChange} inputValue={searchInputValue} />
         <Controls />
       </>
     ) : (
--- src/containers/app.tsx
+++ src/containers/app.tsx
@@ -1,31 +1,48 @@
 import React, { FC, useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { typeOfView } from '../constants/app.constants';
+import { TypeOfView } from '../constants/app.constants';
 import { GlobalStyle } from '../../styles/global-styles';
 import { Header } from '../components/header/header.component';
 import { MoviesList } from '../components/movies-list/movies-list.component';
 import { Footer } from '../components/footer/footer.component';
 import { MoviesSorter } from '../components/movies-sorter/movies-sorter.component';
 import { ErrorBoundaryMoviesList } from './error-boundary';
-import { setViewType, setCurrentMovie, fetchMovies } from '../actions';
+import {
+  setViewType,
+  setCurrentMovie,
+  setSearchInputValue,
+  fetchMovies,
+  fetchMoviesByDate,
+  fetchMoviesByRating,
+} from '../actions';
 import { RootState } from '../store';
 
 export const App: FC = () => {
-  const { viewType, currentMovie, moviesData } = useSelector((state: RootState) => ({
-    viewType: state.viewTypeData,
-    currentMovie: state.currentMovieData,
-    moviesData: state.moviesData.data,
-  }));
+  const { viewType, currentMovie, moviesData, moviesAmount, searchInputValue } = useSelector(
+    (state: RootState) => ({
+      viewType: state.viewTypeData,
+      currentMovie: state.currentMovieData,
+      moviesData: state.moviesData.data,
+      moviesAmount: state.moviesData.data.length,
+      searchInputValue: state.searchInputValueData,
+    })
+  );
   const dispatch = useDispatch();
   const setMovieFullInfoType = useCallback(
     (chosenMovie) => () => {
-      dispatch(setViewType(typeOfView.movieFullInfo));
+      dispatch(setViewType(TypeOfView.movieFullInfo));
       dispatch(setCurrentMovie(chosenMovie));
     },
     []
   );
-  const setMovieListType = useCallback(() => dispatch(setViewType(typeOfView.movieList)), []);
+  const setMovieListType = useCallback(() => dispatch(setViewType(TypeOfView.movieList)), []);
+  const setMovieSortByDate = useCallback(() => dispatch(fetchMoviesByDate()), []);
+  const setMovieSortByRating = useCallback(() => dispatch(fetchMoviesByRating()), []);
+  const setSearchInputChange = useCallback(
+    () => dispatch(setSearchInputValue(searchInputValue)),
+    []
+  );
 
   useEffect(() => {
     dispatch(fetchMovies());
@@ -38,8 +55,14 @@ export const App: FC = () => {
         viewType={viewType}
         currentMovie={currentMovie}
         onSetMovieListType={setMovieListType}
+        onSetSearchInputChange={setSearchInputChange}
+        searchInputValue={searchInputValue}
       />
-      <MoviesSorter />
+      <MoviesSorter
+        moviesAmount={moviesAmount}
+        onSetMovieSortByDate={setMovieSortByDate}
+        onSetMovieSortByRating={setMovieSortByRating}
+      />
       <ErrorBoundaryMoviesList>
         <MoviesList onSetMovieFullInfoType={setMovieFullInfoType} moviesData={moviesData} />
       </ErrorBoundaryMoviesList>
