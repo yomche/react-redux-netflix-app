ac896cda756e8543c46dea448bec4cf110022e0c
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-15 23:04:56 +0300

    add SearchInput props

--- src/components/search-input/search-input.component.tsx
+++ src/components/search-input/search-input.component.tsx
@@ -9,31 +9,38 @@ import {
   StyledSubmitButton,
 } from './search-input.styles';
 
-export const SearchInput: FC = () => (
+type SearchInputProps = {
+  onClickToSearchGenre: () => void;
+  onClickToSearchTitle: () => void;
+  updateForm: (value: string) => void;
+};
+
+export const SearchInput: FC<SearchInputProps> = ({
+  onClickToSearchGenre,
+  onClickToSearchTitle,
+  updateForm,
+}) => (
   <Form
-    onSubmit={(value) => {
+    onSubmit={(value: string) => {
       // eslint-disable-next-line no-console
       console.log(value);
+      updateForm(value);
     }}
-    subscription={{ submitting: true, pristine: true }}
   >
-    {({ handleSubmit, submitting, pristine }) => (
+    {({ handleSubmit }) => (
       <StyledSearchForm onSubmit={handleSubmit}>
         <StyledHeading>FIND YOUR MOVIE</StyledHeading>
         <Field name="movieName" component="input" type="text" placeholder="Quentin Tarantino" />
         <StyledControlsSection>
-          <StyledTitleButton type="button" onClick={() => console.log('title button')}>
+          <StyledTitleButton type="button" onClick={onClickToSearchTitle}>
             {' '}
             TITLE{' '}
           </StyledTitleButton>
-          <StyledGenreButton type="button" onClick={() => console.log('genre button')}>
+          <StyledGenreButton type="button" onClick={onClickToSearchGenre}>
             {' '}
             GENRE{' '}
           </StyledGenreButton>
-          <StyledSubmitButton type="submit" disabled={submitting || pristine}>
-            {' '}
-            SUBMIT{' '}
-          </StyledSubmitButton>
+          <StyledSubmitButton type="submit"> SUBMIT </StyledSubmitButton>
         </StyledControlsSection>
       </StyledSearchForm>
     )}
