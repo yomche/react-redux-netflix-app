fd7544817fad2e55f5c3d5bb5ab2d40807972bd5
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-21 23:43:00 +0300

    delete unused URLs

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -6,7 +6,3 @@ export enum TypeOfView {
 }
 
 export const moviesUrl = 'http://react-cdp-api.herokuapp.com/movies';
-export const moviesUrlSortByDate =
-  'http://react-cdp-api.herokuapp.com/movies?sortBy=release_date&sortOrder=desc';
-export const moviesUrlSortByRating =
-  'http://react-cdp-api.herokuapp.com/movies?sortBy=vote_count&sortOrder=desc';
