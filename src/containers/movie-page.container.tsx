db6f2f5e9755c7a90e1395dd7a9689822ccb3c03
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-29 11:28:11 +0300

    add use effect for fetching movie by id

--- src/containers/movie-page.container.tsx
+++ src/containers/movie-page.container.tsx
@@ -1,23 +1,46 @@
-import React from 'react';
+import React, { FC, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { RootState } from '../store';
+import { fetchMovieById } from '../actions';
 
 import { GlobalStyle } from '../../styles/global-styles';
 import { StyledHeader } from '../components/header/header.styles';
 import { MovieFullInfo } from '../components/movie-full-info/movie-full-info.component';
+import { NavPanel } from '../components/nav-panel/nav-panel.component';
+import { MoviesSorterContainer } from '../containers/movies-sorter.container';
+import { ErrorBoundaryMoviesList } from './error-boundary';
+import { MoviesListContainer } from '../containers/movies-list.container';
+import { Footer } from '../components/footer/footer.component';
 
-export const MoviePageContainer = ({ match }) => {
-  const {
-    params: { movieId },
-  } = match;
+export const MoviePageContainer: FC = () => {
+  const { moviesData } = useSelector((state: RootState) => ({
+    moviesData: state.moviesData.data,
+  }));
 
+  const { movieId } = useParams();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchMovieById(movieId));
+  }, [movieId]);
+
+  const currentMovieById = moviesData.find((currentValue, index, arr) => {
+    console.log(currentValue.id);
+    return currentValue.id == movieId;
+  });
   return (
     <>
-      <p>
-        <strong>User ID: </strong>
-        {movieId}
-      </p>
-      <p>
-        <strong>User Name: </strong>
-      </p>
+      <GlobalStyle />
+      <StyledHeader>
+        <NavPanel />
+        <MovieFullInfo currentMovieFullInfo={currentMovieById} />
+      </StyledHeader>
+      <MoviesSorterContainer />
+      <ErrorBoundaryMoviesList>
+        <MoviesListContainer />
+      </ErrorBoundaryMoviesList>
+      <Footer />
     </>
   );
 };
