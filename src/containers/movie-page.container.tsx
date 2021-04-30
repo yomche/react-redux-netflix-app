dc2f8a2562678334c7a7cf9c5358c8e30375cefd
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-30 14:37:49 +0300

    fix Movie Page container

--- src/containers/movie-page.container.tsx
+++ src/containers/movie-page.container.tsx
@@ -1,45 +1,53 @@
-import React, { FC, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { FC, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { RootState } from '../store';
-import { fetchMovieById } from '../actions';
+import { MoviesDataType } from '../types';
 
 import { GlobalStyle } from '../../styles/global-styles';
 import { StyledHeader } from '../components/header/header.styles';
 import { MovieFullInfo } from '../components/movie-full-info/movie-full-info.component';
 import { NavPanel } from '../components/nav-panel/nav-panel.component';
 import { MoviesSorterContainer } from '../containers/movies-sorter.container';
-import { ErrorBoundaryMoviesList } from './error-boundary';
 import { MoviesListContainer } from '../containers/movies-list.container';
 import { Footer } from '../components/footer/footer.component';
+import { NotFoundContainer } from '../components/not-found/not-found.component';
+import { fetchMovies, setViewType } from '../actions';
+import { TypeOfView } from '../constants/app.constants';
 
 export const MoviePageContainer: FC = () => {
+  const dispatch = useDispatch();
+  const onSetMovieListType = useCallback(() => {
+    dispatch(setViewType(TypeOfView.movieList));
+    dispatch(fetchMovies());
+  }, []);
   const { moviesData } = useSelector((state: RootState) => ({
     moviesData: state.moviesData.data,
   }));
 
-  const { movieId } = useParams();
+  interface ParamTypes {
+    id: string;
+  }
 
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchMovieById(movieId));
-  }, [movieId]);
+  const { id } = useParams<ParamTypes>();
 
-  const currentMovieById = moviesData.find((currentValue, index, arr) => {
-    console.log(currentValue.id);
-    return currentValue.id == movieId;
+  const currentMovieById = moviesData.find((currentValue: MoviesDataType, index, arr) => {
+    return currentValue.id.toString() === id;
   });
+
   return (
     <>
       <GlobalStyle />
       <StyledHeader>
-        <NavPanel />
-        <MovieFullInfo currentMovieFullInfo={currentMovieById} />
+        <NavPanel onClickToChange={onSetMovieListType} />
+        {currentMovieById != undefined ? (
+          <MovieFullInfo currentMovieFullInfo={currentMovieById} />
+        ) : (
+          <NotFoundContainer />
+        )}
       </StyledHeader>
       <MoviesSorterContainer />
-      <ErrorBoundaryMoviesList>
-        <MoviesListContainer />
-      </ErrorBoundaryMoviesList>
+      <MoviesListContainer />
       <Footer />
     </>
   );
