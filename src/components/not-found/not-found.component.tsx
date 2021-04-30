ecc384b59226ef67c8ffa0ec06f5ac892ec4d99c
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-04-30 14:37:00 +0300

    refactor Not Found container

--- src/components/not-found/not-found.component.tsx
+++ src/components/not-found/not-found.component.tsx
@@ -1,14 +1,12 @@
 import React, { FC } from 'react';
 import { StyledPageNotFound, StyledHeading } from './not-found.styles';
-import { Footer } from '../footer/footer.component';
 import { GlobalStyle } from '../../../styles/global-styles';
 
 export const NotFoundContainer: FC = () => (
   <>
     <GlobalStyle />
     <StyledPageNotFound>
-      <StyledHeading> Sorry! Page not found </StyledHeading>
+      <StyledHeading> Not found </StyledHeading>
     </StyledPageNotFound>
-    <Footer />
   </>
 );
