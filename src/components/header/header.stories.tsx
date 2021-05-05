52d85734522897efa5731b6809222eb619fbdc15
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-05-05 22:39:58 +0300

    refactor stories

--- src/components/header/header.stories.tsx
+++ src/components/header/header.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
@@ -16,12 +17,11 @@ storiesOf('Knobs/Header', module)
       poster_path:
         'https://sun9-72.userapi.com/impf/a-V1H8zLHXqzMb3UY8yzgicFlOVZkHw4TNXeLQ/PHLkRY64ehg.jpg?size=1459x2160&quality=96&sign=1063b2565af2a89c8ae442b61427f9e7&type=album',
       title: 'Black Mirror',
-      releaseDate: '2011-02-07',
+      release_date: '2011-02-07',
       overview:
         'Set in a world only minutes from our own, Black Mirror unveils how modern technologies can backfire and be used against their makers, every episode set in a slightly different reality with different characters combating different types of technologies.',
       id: 0,
     };
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
     function setMovieListType() {}
     function setSearchInputValue() {}
     function setMovieSearchType() {}
--- src/components/movies-sorter/movies-sorter.stories.tsx
+++ src/components/movies-sorter/movies-sorter.stories.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import React, { ComponentProps } from 'react';
 
 import { Story } from '@storybook/react';
