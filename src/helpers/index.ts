738b72e6d395b0187153c98a5d4722dc160cf2aa
Ekaterina Smirnova <ekaterina_smirnova1@epam.com>
2021-05-18 23:23:11 +0300

    rename functions for generating URL

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,16 +1,16 @@
 import { moviesUrl } from '../constants/app.constants';
 
-export const generatedUrlByGenre = (sortType: string, movieGenre: string): string => {
+export const generateUrlByGenre = (sortType: string, movieGenre: string): string => {
   const searchURL = `${moviesUrl}?sortBy=${sortType}&sortOrder=desc&search=${movieGenre}&searchBy=genres`;
   return searchURL;
 };
 
-export const generatedUrlBySortType = (sortType: string): string => {
+export const generateUrlBySortType = (sortType: string): string => {
   const searchURL = `${moviesUrl}?sortBy=${sortType}&sortOrder=desc`;
   return searchURL;
 };
 
-export const generatedUrlByInputValue = (
+export const generateUrlByInputValue = (
   sortType: string,
   inputValue: string,
   searchType: string
